035ba23f22894e54830d8b9cf95ac3763b383662
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-05-17 19:06:49 +0530

    Update

--- JMS-First-Client/src/main/java/lk/jiat/ee/jms/App.java
+++ JMS-First-Client/src/main/java/lk/jiat/ee/jms/App.java
@@ -1,19 +1,42 @@
 package lk.jiat.ee.jms;
 
-import jakarta.jms.TopicConnectionFactory;
+import jakarta.jms.*;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
 
         try {
             InitialContext ic = new InitialContext();
             TopicConnectionFactory connectionFactory = (TopicConnectionFactory) ic.lookup("jms/MyConnectionFactory");
             System.out.println(connectionFactory);
-        } catch (NamingException e) {
+
+            TopicConnection connection = connectionFactory.createTopicConnection();
+            connection.start();
+            TopicSession session = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            Topic topic = (Topic) ic.lookup("jms/MyTopic");
+            TopicPublisher publisher = session.createPublisher(topic);
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter your message or type exit: ");
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.equalsIgnoreCase("exit")) {
+                    break;
+                }
+                TextMessage textMessage = session.createTextMessage();
+                textMessage.setText(line);
+                publisher.publish(textMessage);
+
+            }
+
+        } catch (NamingException | JMSException e) {
             throw new RuntimeException(e);
         }
     }
---
+++ JMS-Second-Client/src/main/java/lk/jiat/ee/client/App.java
@@ -0,0 +1,57 @@
+package lk.jiat.ee.client;
+
+import jakarta.jms.*;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Properties;
+
+public class App {
+    public static void main(String[] args) {
+
+
+        try {
+            Properties props = new Properties();
+            props.setProperty(Context.INITIAL_CONTEXT_FACTORY, "com.sun.enterprise.naming.impl.SerialInitContextFactory");
+            props.setProperty(Context.URL_PKG_PREFIXES, "com.sun.enterprise.naming");
+            props.setProperty(Context.STATE_FACTORIES, "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
+
+// Required: Point to the running Payara server (embedded or remote)
+            props.setProperty("org.omg.CORBA.ORBInitialHost", "localhost");
+            props.setProperty("org.omg.CORBA.ORBInitialPort", "3700"); // Payara's default
+
+            InitialContext context = new InitialContext(props);
+            TopicConnectionFactory connectionFactory = (TopicConnectionFactory) context.lookup("jms/MyConnectionFactory");
+
+            TopicConnection connection = connectionFactory.createTopicConnection();
+            connection.start();
+
+            TopicSession session = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+            Topic topic = (Topic) context.lookup("jms/MyTopic");
+
+            TopicSubscriber subscriber = session.createSubscriber(topic);
+
+            Message message = subscriber.receive();
+            System.out.println(message.getBody(String.class));
+
+            subscriber.setMessageListener(new MessageListener() {
+
+                @Override
+                public void onMessage(Message message) {
+                    try {
+                        String msg =message.getBody(String.class);
+                        System.out.println(msg);
+                    } catch (JMSException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+
+            while (true) {}
+
+        } catch (NamingException | JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
